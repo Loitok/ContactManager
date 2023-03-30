0b53118cbc72ad858a7031eb73d7a799aba6740a
Loitok <loitokrozrab@gmail.com>
2023-03-30 20:34:27 +0300

    initial commit

---
+++ ContactManager/Controllers/ContactManagerController.cs
@@ -0,0 +1,24 @@
+﻿using ContactManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager.Controllers
+{
+    public class ContactManagerController : Controller
+    {
+        public ContactManagerController()
+        {
+            
+        }
+
+        public IActionResult UploadCsv()
+        {
+            return View();
+        }
+    }
+}
---
+++ ContactManager/Models/CsvModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ContactManager.Models
+{
+    public class CsvModel
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
